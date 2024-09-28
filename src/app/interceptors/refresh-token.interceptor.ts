18f89ce02dd3a3c7926490282824927af4693e91
DanieleSeverin <danieleseverin89@gmail.com>
2024-09-28 17:28:32 +0200

    refactor RefreshTokenInterceptor

--- src/app/interceptors/refresh-token.interceptor.ts
+++ src/app/interceptors/refresh-token.interceptor.ts
@@ -1,59 +1,68 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { tap, catchError, switchMap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
-import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
-
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable, catchError, throwError, switchMap, forkJoin } from "rxjs";
+import { environment } from "src/environments/environment";
+import { AuthService } from "../services/auth.service";
 
 @Injectable()
 export class RefreshTokenInterceptor implements HttpInterceptor {
-  
-    private isRefreshing = false;
 
-    constructor(private _auth : AuthService, private _router: Router) {}
+    private isRefreshing = false;
+    private requestQueue: HttpRequest<any>[] = [];
+
+    constructor(private _auth: AuthService, private _router: Router) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<any> {
 
-        if(!request.url.startsWith(environment.apiUrl)) {
+        if (!request.url.startsWith(environment.apiUrl)) {
             return next.handle(request);
         }
 
         return next.handle(request).pipe(
             catchError((error) => {
                 if (error instanceof HttpErrorResponse && !request.url.includes('auth/Login') && error.status === 401) {
-                return this.handle401Error(request, next);
+                    return this.handle401Error(request, next);
                 }
-        
+
                 return throwError(() => error);
             })
         );
     }
 
-    private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    private handle401Error(request: HttpRequest<any>, next: HttpHandler): Observable<any> {
         if (!this.isRefreshing) {
             this.isRefreshing = true;
 
-            if (this._auth.isLoggedIn()) {
-                return this._auth.refreshJwt().pipe(
-                    switchMap(() => {
-                        this.isRefreshing = false;
+            // Queue the current request
+            this.requestQueue.push(request);
 
-                        return next.handle(request);
-                    }),
-                    catchError((error) => {
-                        this.isRefreshing = false;
+            return this._auth.refreshJwt().pipe(
+                switchMap(() => {
+                    this.isRefreshing = false;
 
-                        this._auth.logout();
-                        this._router.navigate(['/Login']);
+                    // Retry all the queued requests after token refresh
+                    const retryRequests = this.requestQueue.map(req => next.handle(req));
+                    this.requestQueue = []; // Clear the queue
 
-                        return throwError(() => error);
-                    })
-                );
-            }
+                    return forkJoin(retryRequests);
+                }),
+                catchError((error) => {
+                    this.isRefreshing = false;
+
+                    // On error, clear the queue and navigate to login
+                    this._auth.logout();
+                    this._router.navigate(['/Login']);
+                    this.requestQueue = [];
+
+                    return throwError(() => error);
+                })
+            );
+        } else {
+            // If already refreshing, queue the request and return an empty observable that will be retried later
+            return new Observable(observer => {
+                this.requestQueue.push(request);
+            });
         }
-
-        return next.handle(request);
     }
 }
